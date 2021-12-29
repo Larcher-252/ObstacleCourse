d0cd83efe158018e74a04d2bfe1c01915aa00d17
Void7 <limen191100@gmail.com>
2021-12-29 15:47:16 +0300

    Using methods
    Refactor player's move code to method and call it.

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -4,19 +4,31 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 10;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        PrintInstructions();
     }
-
-    [SerializeField] float moveSpeed = 10;
 
     // Update is called once per frame
     void Update()
     {
-        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed,
-        zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        MovePlayer(moveSpeed);
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game zone.");
+        Debug.Log("Use WASD to move the player.");
+        Debug.Log("Have fun!");
+    }
+
+    void MovePlayer(float speed)
+    {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * speed,
+        zValue = Input.GetAxis("Vertical") * Time.deltaTime * speed;
         transform.Translate(xValue, 0, zValue);
     }
 }
